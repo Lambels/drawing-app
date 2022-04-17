cab96a915f266d99089cad361e5ff8d70f9ebfd3
Lambels <patrick.arvatu@yahoo.com>
2022-04-17 11:29:37 +0300

    implemented server partially

---
+++ server.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+type Server struct {
+	server *http.Server
+	hub    *DrawingHub
+}
+
+func NewServer(port string) *Server {
+	return &Server{
+		server: &http.Server{
+			Addr:    port,
+			Handler: nil,
+		},
+		hub: &DrawingHub{
+			colors: make(map[string]int64),
+			users:  make(map[int64]*User),
+			read:   make(chan *Message),
+		},
+	}
+}
+
+func (s *Server) Open() {
+	s.hub.Open()
+
+	http.HandleFunc("/", s.handleIndex)
+	http.HandleFunc("/ws", s.handleWebsocket)
+
+	if err := s.server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (s *Server) Close() error {
+	s.hub.Close()
+
+	cancelCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	return s.server.Shutdown(cancelCtx)
+}
+
+func (s *Server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "couldnt upgrade connection", http.StatusInternalServerError)
+		return
+	}
+
+	user := &User{
+		conn: socket,
+	}
+
+	s.hub.read <- newJoinMessage(user)
+}
+
+func (s *Server) handleIndex(w http.ResponseWriter, r *http.Request) {
+
+}
