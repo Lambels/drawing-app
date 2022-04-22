04fd660a0ca62fa5c7e901122283230ed0b6ffc1
Lambels <patrick.arvatu@yahoo.com>
2022-04-22 11:02:40 +0300

    changed message structure

--- message.go
+++ message.go
@@ -1,32 +1,97 @@
 package main
 
+type MessageType int
+
 const (
-	TypeUserJoined = iota + 1
+	TypeUserJoined MessageType = iota + 1
 	TypeUserLeft
+	TypeUserFirst
 	TypeClear
 	TypeDraw
 	TypeClose
 )
 
-type Message struct {
-	Type int                         `json:"type"`
-	Data map[interface{}]interface{} `json:"data"`
+type Message interface {
+	Kind() MessageType
+	SenderId() int64
 }
 
-func newJoinMessage(user *User) *Message {
-	msj := Message{
+type MessageUserJoined struct {
+	Type MessageType `json:"type"`
+	User *User       `json:"user"`
+}
+
+func (m *MessageUserJoined) Kind() MessageType {
+	return m.Type
+}
+
+func (m *MessageUserJoined) SenderId() int64 {
+	return -1
+}
+
+func newJoinMessage(user *User) Message {
+	return &MessageUserJoined{
 		Type: TypeUserJoined,
-		Data: make(map[interface{}]interface{}),
+		User: user,
 	}
-	msj.Data[TypeUserJoined] = user
-	return &msj
 }
 
-func newLeaveMessage(id int64) *Message {
-	msj := Message{
+type MessageUserLeft struct {
+	Type MessageType `json:"type"`
+	Id   int64       `json:"id"`
+}
+
+func (m *MessageUserLeft) Kind() MessageType {
+	return m.Type
+}
+
+func (m *MessageUserLeft) SenderId() int64 {
+	return -1
+}
+
+func newLeaveMessage(id int64) Message {
+	return &MessageUserLeft{
 		Type: TypeUserLeft,
-		Data: make(map[interface{}]interface{}),
+		Id:   id,
 	}
-	msj.Data[TypeUserLeft] = id
-	return &msj
+}
+
+type MessageUserFirst struct {
+	Type  MessageType `json:"type"`
+	Id    int64       `json:"id"`
+	Color string      `json:"color"`
+}
+
+func (m *MessageUserFirst) Kind() MessageType {
+	return m.Type
+}
+
+func (m *MessageUserFirst) SenderId() int64 {
+	return m.Id
+}
+
+func newFirstMessage(id int64, color string) Message {
+	return &MessageUserFirst{
+		Type:  TypeUserFirst,
+		Id:    id,
+		Color: color,
+	}
+}
+
+type MessageClose struct {
+	Type MessageType `json:"type"`
+}
+
+func (m *MessageClose) Kind() MessageType {
+	return m.Type
+}
+
+func (m *MessageClose) SenderId() int64 {
+	return -1
+}
+
+func newCloseMessage() Message {
+	return &MessageUserFirst{
+		Type: TypeClose,
+	}
 }
