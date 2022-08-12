811e8a81b55862c5988617268eb602d0dd23cd9c
Lambels <patrick.arvatu@yahoo.com>
2022-08-12 15:05:08 +0300

    gracefull shutdown

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"context"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
-	server := NewServer(":8080")
+	server := NewServer(":8080", 1000)
 	go server.Open()
 
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGQUIT)
 
 	<-c
 
-	server.Close()
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Close(timeoutCtx); err != nil {
+		log.Fatal(err)
+	}
 }
